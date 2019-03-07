dd22f24994d3babf15a7fdaff18ad4e1f74b9e11
Matthew-2033 <matheus-a.g@hotmail.com>
2019-03-07 10:27:09 -0300

    Preparando Banco para realizar avaliações

--- Scripts/PI_3S.sql
+++ Scripts/PI_3S.sql
@@ -1,7 +1,4 @@
-DROP DATABASE PI_3S;
-CREATE DATABASE PI_3S;
-
-USE PI_3S;
+USE hugolutke01;
 
 CREATE TABLE User 
 (
@@ -29,4 +26,75 @@ CREATE TABLE User_Role
 		REFERENCES user(ID_User),
     CONSTRAINT FK_Role_ID FOREIGN KEY(Role_ID)
 		REFERENCES role(Role_ID)        
+);
+
+-- Tables para Realizar a avaliação física:
+CREATE TABLE Avaliado
+(
+	avaliado_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    sexo ENUM('F','M','NC') NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    CPF VARCHAR(15) NOT NULL UNIQUE,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE Avaliacao 
+(
+	avaliacao_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    avaliado_id INT NOT NULL,
+    
+    -- Dados do Avaliado
+	idade INT NOT NULL,
+	massa DECIMAL(4,2) NOT NULL,
+    estatura DECIMAL(4,2) NOT NULL,
+    
+	-- Dobras, unidade de medida em MM
+    peitoral DECIMAL(4,2) NOT NULL,
+    auxiliar_media DECIMAL(4,2) NOT NULL,
+    sub_escapular DECIMAL(4,2) NOT NULL,
+    tricipital DECIMAL(4,2)NOT NULL,
+    biciptal DECIMAL(4,2) NOT NULL,
+    supra_iliaca DECIMAL(4,2) NOT NULL,
+    abdominal DECIMAL(4,2) NOT NULL,
+    coxa DECIMAL(4,2) NOT NULL,
+    panturrilha DECIMAL(4,2) NOT NULL,
+    
+    -- Perimetros, Unidade de medida em Cm
+    torax DECIMAL(4,2) NOT NULL,
+    braco_direito DECIMAL(4,2) NOT NULL,
+    braco_esquerdo DECIMAL(4,2) NOT NULL,
+    antebraco_direito DECIMAL(4,2) NOT NULL,
+    antebraco_esquerdo DECIMAL(4,2) NOT NULL,
+    abdominalUser_Role DECIMAL(4,2) NOT NULL,
+    cintura DECIMAL(4,2) NOT NULL,
+    quadril DECIMAL(4,2) NOT NULL,
+    coxa_direita DECIMAL(4,2) NOT NULL,
+    coxa_esquerda DECIMAL(4,2) NOT NULL,
+    perna_direita DECIMAL(4,2) NOT NULL,
+    perna_esquerda DECIMAL(4,2) NOT NULL,
+    
+    CONSTRAINT avaliacao_dados FOREIGN KEY (avaliado_id)
+		REFERENCES Avaliado(avaliado_id)
+);
+
+CREATE TABLE avaliacao_resultado
+(
+	avaliacao_resultado_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    avaliacao_id INT NOT NULL,
+    avaliado_id INT NOT NULL,
+    imc DECIMAL(4,2) NOT NULL,
+    pccg DECIMAL(4,2) NOT NULL,
+    peso_atual DECIMAL(4,2) NOT NULL,
+    massa_de_gordura DECIMAL(4,2) NOT NULL,
+    massa_magra DECIMAL(4,2) NOT NULL,
+    peso_ideal DECIMAL(4,2) NOT NULL,
+    peso_em_excesso DECIMAL(4,2) NOT NULL,
+    
+    CONSTRAINT resultado_avalicao FOREIGN KEY (avaliacao_id)
+		REFERENCES Avaliacao(avaliacao_id),
+	CONSTRAINT resultado_avaliado FOREIGN KEY (avaliado_id)
+		REFERENCES Avaliado(avaliado_id)
+    
 );
