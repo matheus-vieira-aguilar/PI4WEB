a6a83c73b428596635919adf41a809f6df76b7a2
GabrielolCarmo <gabriel_o.carmo@hotmail.com>
2019-06-16 05:57:32 -0300

    Alteração na chamada da avaliação

--- src/main/resources/static/js/Tabelas/TableDashboardAluno.js
+++ src/main/resources/static/js/Tabelas/TableDashboardAluno.js
@@ -1,16 +1,37 @@
 (function($){
-    var objetoParsed = JSON.parse(objeto)
+    var objetoParsed = objeto;
     var data = objetoParsed.data
 
     $('.table').DataTable( {
         data: data,
         columns: [
-            { data: "dataAvaliacao" },
+            { data: "dataAvaliacao",
+                render: function ( data, type, row, meta ) {
+                    var day = data.dayOfMonth;
+                    var month = data.monthValue - 1;
+                    var year = data.year;
+        
+                    var date = new Date(Date.UTC(year, month, day)).toLocaleDateString();
+                    return date;
+                }    
+            },
             { data: "massa" },
             { data: 'massaDeGordura' },
             { data: "massaMagra" },
             { data: "imc" },
-            { data: "pccg" }
+            { data: "pccg"},
+            {
+                data: "iD", 
+                render: function ( data, type, row, meta ) { 
+                    debugger;
+                    var acoes = '<a class="btn btn-outline-primary fas fas fa-eye" href="/avaliacao/'+ row.idAluno +'?avaliacao=' + row.avaliacao +'" role="button"></a>';
+                    if(row.avaliacao == 1){
+                        acoes = acoes + ' <a class="btn btn-outline-secondary fas fas fa-edit" href="/avaliacao/'+ row.idAluno +'" role="button"></a>'
+                    }            
+
+                    return acoes;
+                } 
+            }
         ]
     } );
 })(jQuery)
